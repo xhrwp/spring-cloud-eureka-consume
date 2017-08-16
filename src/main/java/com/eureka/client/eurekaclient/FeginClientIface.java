ba165385d1a39f4e6bcd47ba1537ae6e49b54f66
xuhongrong <1qaz2wsx!>
2017-08-16 16:51:58 +0800

    11

--- src/main/java/com/eureka/client/eurekaclient/FeginClientIface.java
+++ src/main/java/com/eureka/client/eurekaclient/FeginClientIface.java
@@ -7,13 +7,19 @@ import org.springframework.web.bind.annotation.*;
  * @author 许洪荣
  * @date 2017/8/15
  */
-//名字与配置文件的
+//注意value=服务提供方配置文件的spring.application.name
 @FeignClient(value = "eureka.server.provider")
 public interface FeginClientIface {
 
     @GetMapping(value = "/eureka/testAdd/{id}")
     int add(@PathVariable(value = "id") Integer id);
 
+    /**
+     * 单独参数需要指定　value="userName" 否则服务会报错
+     * @param userName
+     * @param address
+     * @return
+     */
     @GetMapping(value = "/eureka/testStr")
     String testStr(@RequestParam(value = "userName") String userName,@RequestParam(value = "address") String address);
 
