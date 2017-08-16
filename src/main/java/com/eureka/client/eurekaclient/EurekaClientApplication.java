0cab9b25c7f9073bd5955189d59da08f017fab2c
xuhongrong <1qaz2wsx!>
2017-08-16 11:29:39 +0800

    11

--- src/main/java/com/eureka/client/eurekaclient/EurekaClientApplication.java
+++ src/main/java/com/eureka/client/eurekaclient/EurekaClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient	//服务注册于发现
 @EnableCircuitBreaker //熔断器注解
 @EnableFeignClients //feign注解
+@EnableHystrixDashboard	//启用熔断器的dashboard
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
