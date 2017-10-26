28146e1e7eec8760529c754afe0763581d731304
xuhongrong <1qaz2WSX!>
2017-10-26 16:54:56 +0800

    集群测试

--- src/main/java/com/eureka/client/eurekaclient/UserServiceImpl.java
+++ src/main/java/com/eureka/client/eurekaclient/UserServiceImpl.java
@@ -22,11 +22,17 @@ public class UserServiceImpl implements UserService {
 
     /**
      * 熔断
-     *
+     *  HystrixCommand 表明该方法为hystrix包裹，可以对依赖服务进行隔离、降级、快速失败、快速重试等等hystrix相关功能
+     该注解属性较多，下面讲解其中几个
+     fallbackMethod 降级方法
+     commandProperties 普通配置属性，可以配置HystrixCommand对应属性，例如采用线程池还是信号量隔离、熔断器熔断规则等等
+     ignoreExceptions 忽略的异常，默认HystrixBadRequestException不计入失败
+     groupKey() 组名称，默认使用类名称
+     commandKey 命令名称，默认使用方法名
      * 断路器很好理解, 当Hystrix Command请求后端服务失败数量超过一定比例(默认50%),
      * 断路器会切换到开路状态(Open). 这时所有请求会直接失败而不会发送到后端服务. 断路器保持在开路状态一段时间后(默认5秒),
      * 自动切换到半开路状态(HALF-OPEN). 这时会判断下一次请求的返回情况, 如果请求成功, 断路器切回闭路状态(CLOSED), 否则重新切换到开路状态(OPEN).
-     * Hystrix的断路器就像我们家庭电路中的保险丝, 一旦后端服务不可用, 断路器会直接切断请求链, 避免发送大量无效请求影响系统吞吐量, 并且断路器有自我检测并恢复的能力.
+     * 一旦后端服务不可用, 断路器会直接切断请求链, 避免发送大量无效请求影响系统吞吐量, 并且断路器有自我检测并恢复的能力.
      * @param age1
      * @param age2
      * @return
@@ -35,6 +41,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @HystrixCommand(fallbackMethod ="fallback")
     public int add(int age1, int age2) throws Exception {
+        System.out.println("测试123");
         if (age1 == 1) {
             throw new Exception("测试熔断机制");
         }
